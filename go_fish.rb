1b4c1b254399047dc20d26229fb72e4698ff4c25
c00z <zcusimano@gmail.com>
2017-01-19 14:59:19 -0800

    Playing card init passing

--- go_fish.rb
+++ go_fish.rb
@@ -1,20 +1,49 @@
 class PlayingCard
-  # initialize
+
+RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+SUITS = ["C", "D", "H", "S"]
+SORTED_CARDS = [
+  "AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S",
+  "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S",
+  "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S",
+  "10C", "10D", "10H", "10S", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS",
+  "KC", "KD", "KH", "KS"
+]
+
+  attr_reader :rank, :suit, :face
+
+  def initialize(arg)
+    @rank = arg[:rank]
+    @suit = arg[:suit]
+    @face = @rank + @suit
+  end
+
+  def to_s
+    @face
+  end
+
 end
 
+#############
 class CardDeck
-  # initialize
+  attr_accessor
+
+  def initialize()
+  end
+
 end
 
+#############
 class HandOfCards
   # initialize
 end
 
+#############
 class CardPlayer
   # initialize
 end
 
-
+#############
 # Driver Code
 if __FILE__ == $0
   puts "This will only print if you run `ruby go_fish.rb`"
@@ -54,4 +83,6 @@ if __FILE__ == $0
   # end
 
   # puts "Hands: [ #{p1.hand} ], [ #{p2.hand} ] (after)"
+
+
 end
